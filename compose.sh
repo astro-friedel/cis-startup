4fcad81c2c728d4fe987ad16a2510f353e0c41c4
Doug Friedel <friedel@illinois.edu>
2020-07-22 11:30:15 -0500

    fixed path

--- compose.sh
+++ compose.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm --privileged -v $HOME/.docker:/root/.docker -v /var/run/docker.sock:/var/run/docker.sock -v /home/cis/kub/cis-startup/:/workdir -w /workdir docker/compose:1.26.0 "$@"
+docker run -it --rm --privileged -v $HOME/.docker:/root/.docker -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}/cis-startup/:/workdir -w /workdir docker/compose:1.26.0 "$@"
